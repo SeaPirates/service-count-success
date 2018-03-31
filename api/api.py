16fe1336928d4f16a892fa8d391a27dfa0fc453b
Daniel Silva <silva20102@gmail.com>
2018-03-31 20:10:30 -0300

    Adicionado conexão com mysql

--- api/api.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/graphic')
-def api():
-	return 'Minha Primeira API em Python'
---
+++ src/api.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from src.mysql.series import Series
+import os
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASS']
+app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
+app.config['MYSQL_PORT'] = int(os.environ['MYSQL_PORT'])
+mysql = MySQL(app)
+
+@app.route('/graphic', methods=['POST'])
+def api():
+    teste = Series(mysql)
+    return app.response_class(
+        response=teste.metricas(),
+        status=200,
+        mimetype='application/json'
+    )
