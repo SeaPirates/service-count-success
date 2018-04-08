db1b071427d15e0a3b98e187101d867b30268465
Daniel Silva <silva20102@gmail.com>
2018-04-08 15:52:21 -0300

    Update Gitignore

--- src/series/series.py
+++ src/series/series.py
@@ -1,13 +1,31 @@
 from flask import json
+from src.series.exercicios import Exercicios
+import datetime
 
 
 class Series:
 
-    def __init__(self, mysql):
-        self.mysql = mysql
+    def __init__(self):
+        self.exercicios = Exercicios()
 
-    def metricas(self):
-        cursor = self.mysql.connection.cursor()
-        cursor.execute('SELECT * FROM metrics.bancas')
-        result = json.dumps(cursor.fetchall())
-        return result
+    def metricas(self, params):
+        json_params = json.loads(params)
+
+        if (json_params['params']['banca'] == 'all'):
+            result = self.exercicios\
+                .select()\
+                .where(
+                    (self.exercicios.data_registro >= datetime.date(json_params['params']['date_begin'])) &
+                    (self.exercicios.data_registro <= datetime.date(json_params['params']['date_end']))
+                ).count()
+        else:
+            result = self.exercicios \
+                .select() \
+                .where(
+                    (self.exercicios.banca == json_params['params']['banca']) &
+                    (self.exercicios.data_registro >= datetime.date(json_params['params']['date_begin'])) &
+                    (self.exercicios.data_registro <= datetime.date(json_params['params']['date_end']))
+                ).count()
+
+        self.exercicios.close()
+        return json.dumps({"result": str(result)})
